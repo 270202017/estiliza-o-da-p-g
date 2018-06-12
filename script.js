d1afed5d92abc4f398ed46f2a2c00c241057faad
lia <liatoalesp2603@gmail.com>
2018-06-12 13:59:53 -0300

    coloquei js do oseias

---
+++ script.js
@@ -0,0 +1,9 @@
+let botoes = document.querySelectorAll('.btn');
+
+
+function pular(botoes){
+    console.log('subiu');
+    botoes.style.marginBottom ="100px";
+    botoes.style.backgroundColor='black';
+    
+}
